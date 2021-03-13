59de80bb29a86a095134a61303e59547701df535
Randhy <randhypradanairsan@gmail.com>
2021-03-13 11:51:40 +0700

    Initial commit

---
+++ app/src/main/java/com/randhypi/myactionbar/MenuActivity.kt
@@ -0,0 +1,13 @@
+package com.randhypi.myactionbar
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MenuActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_menu)
+    }
+
+
+}
